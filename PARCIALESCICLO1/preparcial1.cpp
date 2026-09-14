f346ad94200fcc12e30800814feefbce91737212
danielrivas10 <00071126@uca.edu.sv>
2026-09-14 12:05:37 -0600

    feat/agregando 1 y 2 parte

---
+++ PARCIALESCICLO1/preparcial1.cpp
@@ -0,0 +1,50 @@
+/*Un equipo de ingenieros realiza un estudio preliminar sobre la estabilidad de una estructura compuesta por diferentes elementos.
+Para cada elemento se registran sus características y tres cargas aplicadas durante diferentes escenarios de prueba.
+El programa deberá registrar los elementos, procesar las cargas, calcular el factor de utilización, determinar el estado de seguridad, identificar el elemento más comprometido, simular un incremento de carga y generar un informe final.
+El sistema trabajará con un máximo de 10 elementos
+*/
+#include <iostream>
+
+struct Elemento {
+
+    std:: string codigo[9], nombre;
+    double longitud, capacidad;
+    float carga[3];
+    bool estadoSeguridad;
+    };
+
+void registrarElemento(Elemento &elemento){
+int opcion;
+
+std:: cout <<"Ingresa cuantos elementos colocarás" << std:: endl;
+std:: cin >> opcion;
+
+if (opcion <1 || opcion >10){
+
+std:: cout <<"Opción invalida" << std::endl;
+
+
+}
+else {
+
+for (int i=0; i<opcion; i++)
+{
+std:: cout <<"Codigo del elemento: " << elemento.codigo << std:: endl;    
+std:: cout <<"Nombre del elemento: " << elemento.nombre << std:: endl;
+std:: cout <<"Cual es su longitud? " << elemento.longitud << std:: endl;
+std:: cout <<"Cuales son sus cargas?: " << elemento.carga << std:: endl;
+std:: cout <<"Cual es su capacidad maxima?: " << elemento.capacidad << std:: endl;
+}
+
+
+};
+
+}
+
+int main (){
+
+    
+
+
+    return 0;
+}
